df3fdf4c84098070406d1276d7b43dbf998e4df6
muratugur348 <70291952+muratugur348@users.noreply.github.com>
2021-04-15 09:49:15 +0300

    Add files via upload

---
+++ Zombie.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Zombie : MonoBehaviour
+{
+    private GameObject hedef;
+    private float mesafe;
+    private UnityEngine.UI.Image can;
+    float sayaccan;
+    private UnityEngine.UI.Text skor;
+    private oyunKontrol oKontrol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oKontrol = GameObject.Find("_Scripts").GetComponent<oyunKontrol>();
+        hedef = GameObject.Find("oyuncu");
+        GameObject imageObject = GameObject.FindGameObjectWithTag("can");
+        can = imageObject.GetComponent<Image>();
+        GameObject textObject = GameObject.FindGameObjectWithTag("skor");
+        skor = textObject.GetComponent<Text>();
+        sayaccan = 0f;
+    }
+    float sayac = 0.5f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if (gameObject.tag == "zombi")
+        {
+            Vector3 rotation = new Vector3(hedef.transform.position.x, transform.position.y,
+               hedef.transform.position.z);
+
+            transform.LookAt(rotation);
+            mesafe = Vector3.Distance(transform.position, hedef.transform.position);
+            if(mesafe>50f)
+                transform.position = Vector3.Lerp(transform.position,
+                hedef.transform.position, 0.008f);
+            else
+            transform.position = Vector3.Lerp(transform.position,
+                hedef.transform.position, 0.03f);
+           
+            mesafe = Vector3.Distance(transform.position, hedef.transform.position);
+            if (mesafe < 3.5f)
+            {
+                
+                gameObject.GetComponentInChildren<Animation>().Play("Zombie_Attack_01");
+                sayac = 0.5f;
+                sayaccan -= Time.deltaTime;
+                if(sayaccan<0)
+                {
+                can.fillAmount -= 0.25f;
+                    sayaccan = 1.6f;
+                }
+                if(can.fillAmount<=0)
+                {
+                    oKontrol.bittimi = true;
+                        int highskor = PlayerPrefs.GetInt("skor");
+                        if (int.Parse(skor.text) > highskor)
+                        {
+                            PlayerPrefs.SetInt("skor", int.Parse(skor.text));
+                        }
+                }
+                
+            }
+            else if (mesafe > 3.5f)
+            {
+                sayaccan = 0f;
+                sayac -= Time.deltaTime;
+                if (sayac < 0)
+                {
+                    gameObject.GetComponentInChildren<Animation>().Play("Zombie_Walk_01");
+
+                }
+            }
+        }
+        
+       
+    }
+}
---
+++ oyuncuKontrol.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyuncuKontrol : MonoBehaviour
+{
+    public GameObject silah;
+    public GameObject mermi;
+    public Transform mermiPos;
+    public GameObject atesefekt;
+    float sayac;
+    bool isClicked = false;
+    public UnityEngine.UI.Image can;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sayac = 0f;
+        silah.GetComponent<Animator>().enabled = false;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            silah.GetComponent<Animator>().enabled = true;
+            silah.GetComponent<Animator>().Play(0);
+            
+            
+            isClicked = true;
+            
+           
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            isClicked = false;
+            sayac = 0f;
+            silah.GetComponent<Animator>().enabled = false;
+            silah.transform.localPosition= new Vector3(0.8000031f, -1.35f, 0.9100037f);
+            silah.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+           
+        }
+        if(isClicked==true)
+        {
+            sayac -= Time.deltaTime;
+            if (sayac < 0)
+            {
+                GameObject mermiClone = Instantiate(mermi, mermiPos.position, mermiPos.rotation) as GameObject;
+                GameObject ates = Instantiate(atesefekt, mermiPos.position, mermiPos.rotation) as GameObject;
+
+                mermiClone.GetComponent<Rigidbody>().velocity = mermiPos.transform.up * 100f;
+                sayac = 0.2f;
+                
+                Destroy(mermiClone, 1f);
+                Destroy(ates, 1f);
+            }
+        }
+        
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag.Equals("kalp"))
+        {
+            if (can.fillAmount < 1) { 
+            Destroy(other.gameObject);
+
+            can.fillAmount += 0.15f;
+        }
+            
+        }
+    }
+    public void scriptkapat()
+    {
+        foreach (Behaviour childCompnent in GetComponentsInChildren<Behaviour>())
+        {
+            if(childCompnent is Animation)
+            { }
+            else if(childCompnent is AudioSource)
+            {
+
+            }
+            else if(childCompnent is Camera)
+            { }
+            else if(childCompnent is AudioListener)
+            { }
+            else
+            childCompnent.enabled = false;
+        }
+
+    }
+
+
+}
